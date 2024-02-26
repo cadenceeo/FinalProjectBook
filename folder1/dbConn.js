27b8b69455f117df0395f66f7d18a697ce26e6dc
Cadence Ohl <112006923+cadenceeo@users.noreply.github.com>
2024-02-26 08:20:03 -0600

    Update dbConn.js

--- folder1/dbConn.js
+++ folder1/dbConn.js
@@ -1,1 +1,21 @@
+/**
+ * This file is for instantiating the mongoose package to connect
+ * our application's server to the MongoDB database. 
+ * Our .env file includes the DATABASE name and password
+ */
 
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+        // connect to mongoDB via DATABASE URI in .env file (local only)
+        await mongoose.connect(process.env.DATABASE_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true
+        });
+    } catch (err) {
+        console.error(err);
+    }
+}
+
+module.exports = connectDB
